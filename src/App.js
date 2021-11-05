7929855e13a93a7298086c718d8d8254de66c9c4
yonas34 <50245610+yonas34@users.noreply.github.com>
2021-11-05 02:42:50 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Routes from './Routes';
+import {useSelector} from "react-redux"
+import Login from "./pages/Login"
 
 function App() {
+
+  const isLoggedin = useSelector(state =>state.isLoggedin);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {isLoggedin? <Routes/>:<Login/>}
     </div>
   );
 }
