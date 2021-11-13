9b0740be19baa5f296195073fc84909afb6a15cf
yonas34 <50245610+yonas34@users.noreply.github.com>
2021-11-13 04:39:29 -0300

    login fixed

--- src/App.js
+++ src/App.js
@@ -9,31 +9,11 @@ import React,{useEffect}  from 'react'
 import axios from 'axios'
 import {useHistory} from 'react-router-dom'
 function App() {
-  const dispatch = useDispatch()
-  const history=useHistory();
-  console.log(localStorage.length);
-  useEffect(async() => {
-    const response=JSON.parse(localStorage.getItem("auth"));
-    
-    if(response && localStorage.length>0)
-    {
-     
-        await axios.post('https://www.nrwlpms.com/api/api/login.php',{
-            "user_name": response.user_name,
-            "password": response.pass
-        }).then((response_api)=>{
-            const data={username:response_api.data.data.user_name,token:response_api.data.jwt,userId:response_api.data.data.emp_id,usertype:response_api.data.data.user_type_id}
-
-            dispatch(setUser(data));
-           history.push('/');
-         
-           
-    })
-   
-}}, [])
+  
 
 
   const isLoggedin = useSelector(state =>state.isLoggedin);
+  console.log(isLoggedin);
   return (
     <div>
      {isLoggedin? <Sidebar router={<Routes/>}/> :<Login/>}
--- src/index.js
+++ src/index.js
@@ -7,14 +7,40 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {createStore} from "redux";
 import allReducers from "./reducers/";
 import { Provider } from 'react-redux';
+import {useDispatch } from 'react-redux'
+import { loginToggle,setUser } from './reducers/actions'
+import {useSelector} from "react-redux"
+import axios from 'axios'
 const store=createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+
+const isLoggedin = store.getState(state =>state.isLoggedin);
+
+const response=JSON.parse(localStorage.getItem("auth"));
+    
+if(localStorage.length>0)
+{console.log(response)
+
+     axios.post('https://www.nrwlpms.com/api/api/login.php',{
+        "user_name": response.user_name,
+        "password": response.pass
+    }).then((response_api)=>{
+        const data={username:response_api.data.data.user_name,token:response_api.data.jwt,userId:response_api.data.data.emp_id,usertype:response_api.data.data.user_type_id}
+
+        store.dispatch(setUser(data));
+        if(!isLoggedin.isLoggedin) 
+         store.dispatch(loginToggle());
+
+     
+       
+})}
 
 
 
 ReactDOM.render(
   <Provider store={store}>
   <Router>
+
     <App />
     </Router>
     
